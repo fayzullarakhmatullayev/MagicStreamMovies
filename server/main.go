329dad726131bc0303f9a5d6a94c0e69aae3faae
Fayzulla Raxmatullayev <fayzullarakhmatullayev@gmail.com>
2025-10-07 12:26:37 +0500

    feat: movies controller

--- server/main.go
+++ server/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 
+	"github.com/fayzullarakhmatullayev/MagicStreamMovies/server/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
+	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, GO!")
 	})
+
+	router.GET("/movies", controllers.GetMovies())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
